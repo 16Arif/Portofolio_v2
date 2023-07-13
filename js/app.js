28d69bde56a177309ce0324a6255c715d5eb725f
abdul_arif <arif.abdul0002@gmail.com>
2023-07-13 23:33:23 +0800

    add back to top feature

--- js/app.js
+++ js/app.js
@@ -18,3 +18,25 @@ $('.carousel .carousel-item').each(function () {
     next.children(':first-child').clone().appendTo($(this));
     
     for (var i = 0; i < minPerSlide; i++) { next=next.next(); if (!next.length) { next=$(this).siblings(':first'); } next.children(':first-child').clone().appendTo($(this)); } });
+
+    // back to top 
+    let calcScrollValue = () =>{
+        let scrollProgress = document.getElementById
+        ("progress");
+        let progressValue = document.getElementById
+        ("progress-value");
+        let pos = document.documentElement.scrollTop;
+        let calcHeight = document.documentElement.clientHeight - document.documentElement.clientHeight;
+        let scrollValue = Math.round((pos * 100) / calcHeight);
+        if (pos > 100) {
+            scrollProgress.style.display = "grid";
+        }else {
+            scrollProgress.style.display = "none";
+        }
+        scrollProgress.addEventListener("click", () =>{
+            document.documentElement.scrollTop = 0;
+        }); 
+        scrollProgress.style.background = `conic-gradient (#3139a8 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+    };
+    window.onscroll = calcScrollValue;
+    window.onload = calcScrollValue;
